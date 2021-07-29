278e6cfba70467c96a78535f9671f46ddb640ad5
SueTheCoder <suheyla315@hotmail.com>
2021-07-29 17:30:22 -0400

    Task's done

--- src/test/java/com/ZeroBank/runners/CukesRunner.java
+++ src/test/java/com/ZeroBank/runners/CukesRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/zeroBank-report.html",
-                "json:target/cucumber.json",
+        plugin = {"json:target/cucumber.json",
+                "html:target/zeroBank-report.html",
                 "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
